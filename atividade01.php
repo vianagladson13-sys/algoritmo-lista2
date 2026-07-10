b5955a8980ac8a259f4e6b1d082f9b38c3e543dc
Gladson viana <gladsonviana13@gmail.com>
2026-07-10 20:08:55 -0300

    atualizado

--- atividade01.php
+++ atividade01.php
@@ -14,19 +14,22 @@ Uma pessoa comprou arroz, feijão e leite. Armazene e calcule
 ● Se o total da compra for maior que R$ 100,00, exiba: “Você ganhou um
 desconto de R$ 10,00.” 
 */
-$Produto1 = "arroz";
+$Produto1 = "Arroz";
 $preco1 = 25.00;
 $quantidade1 = 10;
+$TotalProduto1 = $preco1 * $quantidade1;
 
-$Produto2 = "feijão";
+$Produto2 = "Feijão";
 $preco2 = 10.00;
 $quantidade2 = 10;
+$TotalProduto2 = $preco2 * $quantidade2;
 
-$Produto3 = "leite";
+$Produto3 = "Leite";
 $preco3 = 4.50;
 $quantidade3 = 10;
+$TotalProduto3 = $preco3 * $quantidade3;
 
-$ValortotalCompra = ($preco1 * $quantidade1) + ($preco2 * $quantidade2) + ($preco3 * $quantidade3);
+$ValortotalCompra = $TotalProduto1 + $TotalProduto2 + $TotalProduto3;
 
 $desconto = 0;
 
@@ -48,6 +51,15 @@ $valorTotalcomDesconto = $ValortotalCompra - $desconto;
 </head>
 
 <body>
+
+<h2>Notinha fiscal - Compras </h2>
+<br>
+<p><?php echo("PRODUTO: " . $Produto1 . " --> QUANTIDADE: " . $quantidade1 . " -->PREÇO UNITARIO " . $preco1 . " -->TOTAL PRODUTO: " . $TotalProduto1 ); ?></p>
+<p><?php echo("PRODUTO: " . $Produto2 . " --> QUANTIDADE: " . $quantidade2 . " -->PREÇO UNITARIO " . $preco2 . " -->TOTAL PRODUTO: " . $TotalProduto2 ); ?></p>
+<p><?php echo("PRODUTO: " . $Produto3 . " --> QUANTIDADE: " . $quantidade3 . " -->PREÇO UNITARIO " . $preco3 . " -->TOTAL PRODUTO: " . $TotalProduto3 ); ?></p>
+
+
+
 
 </body>
 
