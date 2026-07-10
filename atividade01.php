3e3f5eab93217b0fa3e308f6bd256f05e29058ec
Gladson viana <gladsonviana13@gmail.com>
2026-07-10 20:17:41 -0300

    ATUALIZADO

--- atividade01.php
+++ atividade01.php
@@ -54,9 +54,9 @@ $valorTotalcomDesconto = $ValortotalCompra - $desconto;
 
     <h2>Notinha fiscal - Compras </h2>
     <br>
-    <p><?php echo ("PRODUTO: " . $Produto1 . " -> QUANTIDADE: " . $quantidade1 . " -> PREÇO UNITARIO " . $preco1 . " -> TOTAL PRODUTO: " . $TotalProduto1); ?></p>
-    <p><?php echo ("PRODUTO: " . $Produto2 . " -> QUANTIDADE: " . $quantidade2 . " -> PREÇO UNITARIO " . $preco2 . " -> TOTAL PRODUTO: " . $TotalProduto2); ?></p>
-    <p><?php echo ("PRODUTO: " . $Produto3 . " -> QUANTIDADE: " . $quantidade3 . " -> PREÇO UNITARIO " . $preco3 . " -> TOTAL PRODUTO: " . $TotalProduto3); ?></p>
+    <p><?php echo ("PRODUTO: " . $Produto1 . " - QUANTIDADE: " . $quantidade1 . " - PREÇO UNITARIO " . $preco1 . " - TOTAL PRODUTO: " . $TotalProduto1); ?></p>
+    <p><?php echo ("PRODUTO: " . $Produto2 . " - QUANTIDADE: " . $quantidade2 . " - PREÇO UNITARIO " . $preco2 . " - TOTAL PRODUTO: " . $TotalProduto2); ?></p>
+    <p><?php echo ("PRODUTO: " . $Produto3 . " - QUANTIDADE: " . $quantidade3 . " - PREÇO UNITARIO " . $preco3 . " - TOTAL PRODUTO: " . $TotalProduto3); ?></p>
     <br>
     <p><?php echo ("VALOR TOTAL: " . $ValortotalCompra); ?></p>
     <p><?php echo ("VALOR DESCONTO: " . $desconto); ?></p>
