795c169517a0af85ef642e40394140fedc936c07
Gladson viana <gladsonviana13@gmail.com>
2026-07-10 21:45:47 -0300

    atualizado

---
+++ exercicio3.php
@@ -0,0 +1,40 @@
+<?php
+/* Uma pessoa realizou uma viagem de carro. Armazene e calcule
+● a distância percorrida.
+● a quantidade de combustível consumida.
+● Calcule o consumo médio: consumo médio = distância ÷ combustível
+● Exiba a distância, o combustível gasto e o consumo médio.
+● Se o consumo médio for maior ou igual a 12 km/l, exiba: O veículo teve um bom
+consumo.
+*/
+
+$DistanciaPercorrida = 50;
+$QtdcombustivelConsumido = 3;
+
+$ConsumoMedio = $DistanciaPercorrida / $QtdcombustivelConsumido;
+$Distancia = $QtdcombustivelConsumido + $ConsumoMedio;
+$frase = "";
+if ($ConsumoMedio>= 12 ) {
+    $frase = "o veiculo teve um bom consumo";
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consumo</title>
+</head>
+
+<body>
+<h3>Media de consumo </h3>
+<p><?php echo (" Distancia percorrida: " . $DistanciaPercorrida);  ?></p>
+<p><?php echo (" Combustivel gasto: " . $QtdcombustivelConsumido);  ?></p>
+<P><?php echo (" Consumo medio: " . $ConsumoMedio);  ?></P>
+<P><?php echo ("Frase: " . $frase);  ?></P>
+</body>
+
+</html>
